917815ce04426ae70e8a5cb0d7a3d67b9fcb8366
Markus Steinberg <markus.daniel.steinberg@uni-jena.de>
2018-04-27 12:32:25 +0200

    Added annotation suggestion generation

---
+++ Console/BExIS.Web.Shell/Areas/DCM/Models/OntologyMappingSuggestion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BExIS.Modules.Dcm.UI.Models
+{
+    public class OntologyMappingSuggestionModel
+    {
+        public string conceptURI;
+        public string displayName;
+        public double similarity;
+
+        public OntologyMappingSuggestionModel(string uri, string label, double sim)
+        {
+            this.conceptURI = uri;
+            this.displayName = label;
+            this.similarity = sim;
+        }
+    }
+}
