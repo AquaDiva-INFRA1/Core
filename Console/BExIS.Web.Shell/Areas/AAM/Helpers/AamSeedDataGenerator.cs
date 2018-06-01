454033939a2bf5651ad2c736126ea16aec089003
Markus Steinberg <markus.daniel.steinberg@uni-jena.de>
2018-06-01 11:06:55 +0200

    [Unstable]WIP on AAM

---
+++ Console/BExIS.Web.Shell/Areas/AAM/Helpers/AamSeedDataGenerator.cs
@@ -0,0 +1,36 @@
+﻿using BExIS.Security.Entities.Objects;
+using BExIS.Security.Services.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BExIS.Modules.Bam.UI.Helpers
+{
+    public class AamSeedDataGenerator
+    {
+        public static void GenerateSeedData()
+        {
+            FeatureManager featureManager = new FeatureManager();
+            OperationManager operationManager = new OperationManager();
+
+            try
+            {
+                #region Security
+                List<Feature> features = featureManager.FeatureRepository.Get().ToList();
+
+                Feature AquadivaAnnotationsFeature = features.FirstOrDefault(f => f.Name.Equals("Aquadiva Annotations"));
+                if (AquadivaAnnotationsFeature == null) AquadivaAnnotationsFeature = featureManager.Create("Aquadiva Annotations", "Aquadiva Annotations");
+
+                operationManager.Create("AAM", "Help", "*");
+                operationManager.Create("AAM", "Annotation", "*", AquadivaAnnotationsFeature);
+                #endregion
+            }
+            finally
+            {
+                featureManager.Dispose();
+                operationManager.Dispose();
+            }
+        }
+    }
+}
---
+++ Modules/AAM/BExIS.Aam.Entities/Mapping/Annotation.cs
@@ -0,0 +1,44 @@
+﻿using BExIS.Dlm.Entities.Data;
+using BExIS.Dlm.Entities.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vaiona.Entities.Common;
+
+namespace BExIS.Aam.Entities.Mapping
+{
+    public class Annotation
+    {
+        public virtual Dataset Dataset { get; set; }
+        public virtual DatasetVersion DatasetVersion { get; set; }
+        public virtual Variable Variable { get; set; }
+        public virtual String Entity { get; set; }
+        public virtual String Characteristic { get; set; }
+        public virtual String Standard { get; set; }
+        public virtual long EntityId { get; set; }
+        public virtual long CharacteristicId { get; set; }
+        public virtual long StandardId { get; set; }
+        
+        public Annotation(Dataset ds, DatasetVersion dsv, Variable var, String entity, String characteristic)
+        {
+            this.Dataset = ds;
+            this.DatasetVersion = dsv;
+            this.Variable = var;
+            this.Entity = entity;
+            this.Characteristic = characteristic;
+            this.Standard = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#Standard"; //Default standard
+        }
+
+        public Annotation(Dataset ds, DatasetVersion dsv, Variable var, String entity, String characteristic, String standard)
+        {
+            this.Dataset = ds;
+            this.DatasetVersion = dsv;
+            this.Variable = var;
+            this.Entity = entity;
+            this.Characteristic = characteristic;
+            this.Standard = standard;
+        }
+    }
+}
