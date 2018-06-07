36b04efbe94254d277cb7b22b63322c860ab6181
Markus Steinberg <markus.daniel.steinberg@uni-jena.de>
2018-06-07 14:46:22 +0200

    AAM: Reworked and tested create functions

--- Console/BExIS.Web.Shell/Areas/AAM/Controllers/AnnotationController.cs
+++ Console/BExIS.Web.Shell/Areas/AAM/Controllers/AnnotationController.cs
@@ -1,6 +1,7 @@
 ﻿using BExIS.Aam.Entities.Mapping;
 using BExIS.Aam.Services;
 using BExIS.Dlm.Entities.Data;
+using BExIS.Dlm.Entities.DataStructure;
 using BExIS.Dlm.Services.Data;
 using BExIS.Modules.Aam.UI.Models;
 using System;
@@ -9,6 +10,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vaiona.Persistence.Api;
 
 namespace BExIS.Modules.Bam.UI.Controllers
 {
@@ -18,7 +20,6 @@ namespace BExIS.Modules.Bam.UI.Controllers
         public ActionResult Index()
         {
             AnnotationManager am = new AnnotationManager();
-            List<Annotation> annotationList = am.GetAnnotations().OrderBy(an => an.Dataset.Id).ToList();
 
             #region Test functions
             //Testing get-functions
@@ -30,10 +31,25 @@ namespace BExIS.Modules.Bam.UI.Controllers
             //am.DeleteAnnotation(12345);
             //am.DeleteAnnotation(new List<long>(new long[] {12346, 12347, 12348}));
             //am.DeleteAnnotationsForDataset(199);
-            am.DeleteAnnotationsForDatasetVersion(5000);
+            //am.DeleteAnnotationsForDatasetVersion(5000);
 
+            //Testing Create functions
+            /*DatasetManager dsm = new DatasetManager();
+            Dataset testDataset = dsm.GetDataset(199);
+            DatasetVersion testDsv = dsm.GetDatasetLatestVersion(testDataset);
+            Variable testVariable;
+            using(IUnitOfWork uow = this.GetUnitOfWork())
+            {
+                IReadOnlyRepository<Variable> repo = uow.GetReadOnlyRepository<Variable>();
+                testVariable = repo.Get(2476);
+            }*/
+            //am.CreateAnnotation(testDataset, testDsv, testVariable, "Test Entity", "Test Characteristic", "Test Standard");
+            //am.CreateAnnotation(199, 446, testVariable, "Test Entity", "Test Characteristic", "Test Standard");
+            //am.CreateAnnotation(199, 446, testVariable, "Test Entity", "Test Characteristic");
+            //am.CreateAnnotation(testDataset, testDsv, testVariable, "Test Entity", "Test Characteristic");
             #endregion
 
+            List<Annotation> annotationList = am.GetAnnotations().OrderBy(an => an.Dataset.Id).ToList();
             List<AnnotationModel> model = new List<AnnotationModel>();
             foreach(Annotation an in annotationList)
             {
