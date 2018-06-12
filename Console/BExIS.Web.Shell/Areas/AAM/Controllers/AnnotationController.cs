de3cabb1aceef355a90be98f7912fc8d5ec6a6c9
Markus Steinberg <markus.daniel.steinberg@uni-jena.de>
2018-06-12 07:25:27 +0200

    Changed namespaces from BAM to AAM

--- Console/BExIS.Web.Shell/Areas/AAM/Controllers/AnnotationController.cs
+++ Console/BExIS.Web.Shell/Areas/AAM/Controllers/AnnotationController.cs
@@ -12,7 +12,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vaiona.Persistence.Api;
 
-namespace BExIS.Modules.Bam.UI.Controllers
+namespace BExIS.Modules.Aam.UI.Controllers
 {
     public class AnnotationController : Controller
     {
