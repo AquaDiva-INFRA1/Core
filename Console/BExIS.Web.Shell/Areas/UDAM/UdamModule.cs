51a16bfc4ff4c82dcd4aa1b0ff57ae322fb06efb
hamdi1992 <hamdi.hamed1992@gmail.com>
2018-07-17 10:13:39 +0200

    unstructred data analysis process 1

---
+++ Console/BExIS.Web.Shell/Areas/UDAM/UdamModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Vaiona.Logging;
+using Vaiona.Web.Mvc.Modularity;
+
+namespace BExIS.Modules.Sam.UI
+{
+    public class UdamModule : ModuleBase
+    {
+        public UdamModule() : base("udam")
+        {
+            LoggerFactory.GetFileLogger().LogCustom("...ctor of udam...");
+        }
+
+        public override void Install()
+        {
+            LoggerFactory.GetFileLogger().LogCustom("...start install of udam...");
+            try
+            {
+                base.Install();
+
+                //using (var generator = new SamSeedDataGenerator())
+                //{
+                //    generator.GenerateSeedData();
+                //}
+            }
+            catch (Exception e)
+            {
+                LoggerFactory.GetFileLogger().LogCustom(e.Message);
+                LoggerFactory.GetFileLogger().LogCustom(e.StackTrace);
+            }
+
+            LoggerFactory.GetFileLogger().LogCustom("...end install of udam...");
+        }
+    }
+}
