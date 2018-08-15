70369bf0e4b8389542be2d7b78d892ce66f6a688
hamdi1992 <hamdi.hamed1992@gmail.com>
2018-08-15 15:42:06 +0200

    analytics and statistics module integrated

---
+++ Console/BExIS.Web.Shell/Areas/ASM/Models/data_container_analytics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BExIS.Modules.Asm.UI.Models
+{
+    public class Data_container_analytics
+    {
+        //var templates : use non-use results
+        public int in_use_var_temps ;
+        public int not_used_var_temps ;
+
+        //units : use non-use results
+        public int in_use_unit ;
+        public int not_used_unit ;
+
+        //Data types : use non-use results
+        public int in_use_DataType;
+        public int not_used_DataType;
+
+        public Data_container_analytics(int in_use_var_temps, int not_used_var_temps, 
+            int in_use_unit, int not_used_unit,
+            int in_use_DataType, int not_used_DataType)
+        {
+            this.in_use_var_temps = in_use_var_temps;
+            this.not_used_var_temps = not_used_var_temps;
+
+            this.in_use_unit = in_use_unit;
+            this.not_used_unit = not_used_unit;
+
+            this.in_use_DataType = in_use_DataType;
+            this.not_used_DataType = not_used_DataType;
+        }
+    }
+}
