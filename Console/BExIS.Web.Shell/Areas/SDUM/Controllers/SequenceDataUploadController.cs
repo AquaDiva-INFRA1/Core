1ebb251db8306a838b00209258f33c6b006cef10
hamdi1992 <hamdi.hamed1992@gmail.com>
2019-01-24 10:00:47 +0100

    initialisation of SDUM

---
+++ Console/BExIS.Web.Shell/Areas/SDUM/Controllers/SequenceDataUploadController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Diagnostics;
+using BExIS.Dlm.Services.Data;
+using Npgsql;
+using System.Configuration;
+using BExIS.Dlm.Services.DataStructure;
+using BExIS.Dlm.Entities.DataStructure;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using BExIS.Modules.Asm.UI.Models;
+using BExIS.Modules.Rpm.UI.Models;
+using System.Linq;
+using System.Xml;
+using BExIS.Aam.Services;
+using BExIS.Aam.Entities.Mapping;
+using BExIS.Modules.Rpm.UI.Controllers;
+using BExIS.IO.Transform.Output;
+using BExIS.Utils.Models;
+using System.Data;
+
+namespace BExIS.Modules.Sdum.UI.Controllers
+{
+    public class SequenceDataUploadController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        
+    }
+    
+}
