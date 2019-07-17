7d382261a1ea51086ba5771ed7c7791c1ac7950b
Hamdi1992 <hamdi.hamed1992@gmail.com>
2019-07-17 15:03:19 +0200

    ASM: seed data fixed.

--- Console/BExIS.Web.Shell/Areas/ASM/AsmModule.cs
+++ Console/BExIS.Web.Shell/Areas/ASM/AsmModule.cs
@@ -1,5 +1,7 @@
 ﻿using Vaiona.Logging;
 using Vaiona.Web.Mvc.Modularity;
+using BExIS.Modules.Asm.UI.Helpers;
+using System;
 
 namespace BExIS.Modules.Asm.UI
 {
@@ -10,5 +12,38 @@ namespace BExIS.Modules.Asm.UI
             LoggerFactory.GetFileLogger().LogCustom("...ctor of asm...");
         }
 
+        public override void Install()
+        {
+            LoggerFactory.GetFileLogger().LogCustom("...start install of ddm...");
+            try
+            {
+                base.Install();
+                AsmSeedDataGenerator.GenerateSeedData();
+            }
+            catch (Exception e)
+            {
+                LoggerFactory.GetFileLogger().LogCustom(e.Message);
+                LoggerFactory.GetFileLogger().LogCustom(e.StackTrace);
+            }
+
+            LoggerFactory.GetFileLogger().LogCustom("...end install of ddm...");
+
+
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Shutdown()
+        {
+            base.Shutdown();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
     }
 }
